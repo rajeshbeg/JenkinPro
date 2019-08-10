2b5fa8334a40d8a872647991fd0830b4f5ba8352
rajesh <rajesh@DESKTOP-MCNIVSJ>
2019-08-11 00:13:36 +0530

    Version 1.0

---
+++ src/main/java/JenkinInti/jenkinpro/App.java
@@ -0,0 +1,13 @@
+package JenkinInti.jenkinpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/JenkinInti/jenkinpro/MySchool.java
@@ -0,0 +1,12 @@
+package JenkinInti.jenkinpro;
+
+import org.testng.annotations.Test;
+
+public class MySchool {
+	
+	@Test
+	public void test() {
+		System.out.println("I Am Learning Jenkin");
+	}
+
+}
